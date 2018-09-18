879b712518bddf88846bf89ed6c80b0ba33c3559
Arun <anekkal@iu.edu>
2018-09-18 13:42:24 -0400

    Base configuration | Material Bootstrap, Angular Material, added shared module [Header, Footer]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {  SharedModule  } from './shared/shared.module'
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
